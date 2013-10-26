f7e50a0b991fd295a6884284da25b4b7d41c699f
Devin Brown <devin.g.brown@gmail.com>
2013-10-25 20:27:57 -0600

    oops!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrndzApp::Application.config.secret_key_base = 'f9b57b1cf4c06d4d56f86078a67ff031ea539fd7325358f387f6730d95b19f8fa535f7368cfa64c1ce08a2180c6b26a327f843cabc864ab333831a2c407cb070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrndzApp::Application.config.session_store :cookie_store, key: '_grndz-app_session'
