91cec9cfeda01d50455b57eba5cc73e0b43b6f19
Devin Brown <devin.g.brown@gmail.com>
2013-11-02 17:21:33 -0600

    remove unused shit

--- spec/views/layouts/application_spec.rb
+++ spec/views/layouts/application_spec.rb
@@ -5,11 +5,6 @@ describe "layouts/application" do
     render
   end
 
-  it "should have the correct page title" do
-    title = "Grndz - Craft Coffee Redefined - High quality roasts, online marketplace."
-    response.should have_content title
-  end
-
   it "should render the google analytics partial" do
     response.should render_template "shared/_google_analytics"
   end
--- spec/views/shared/index_spec.rb
+++ spec/views/shared/index_spec.rb
@@ -5,11 +5,4 @@ describe 'layouts/index' do
   before(:each) do
     render
   end
-
-  it "should render some titles (e.g. it exists)" do
-    expect(rendered).to include("The Promise.")
-    expect(rendered).to include("A coffee selection that you can understand.")
-    expect(rendered).to include("Coffee is delivered directly to your door.")
-    expect(rendered).to include("A seriously dependable coffee subscription service.")
-  end
 end
