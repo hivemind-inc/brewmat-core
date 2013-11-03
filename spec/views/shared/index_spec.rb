82be119af44357f7f271de90bf2ea1ef13c382c4
Devin Brown <devin.g.brown@gmail.com>
2013-11-02 20:59:10 -0600

    SexyHomepage. #13

--- spec/views/shared/index_spec.rb
+++ spec/views/shared/index_spec.rb
@@ -5,4 +5,12 @@ describe 'layouts/index' do
   before(:each) do
     render
   end
+
+  it "should have a title" do
+    rendered.should have_content "We Build Coffee."
+  end
+
+  it "should have a subtitle" do
+    rendered.should have_content "A coffee marketplace that you can actually understand."
+  end
 end
