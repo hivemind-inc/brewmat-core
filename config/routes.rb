d4ebe9600d312187439ec79bfa2d7c06eb2efa64
Devin Brown <devin.g.brown@gmail.com>
2014-03-03 14:18:54 -0700

    Devise + login + signup + edit account + misc. styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 GrndzApp::Application.routes.draw do
+  devise_for :users
   root to: 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
