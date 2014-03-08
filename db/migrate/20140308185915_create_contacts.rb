37677f5617c4f024303cf51661cd5eb6a7210912
Devin Brown <devin.g.brown@gmail.com>
2014-03-08 12:14:05 -0700

    add contacts table + associations w/ the user

---
+++ db/migrate/20140308185915_create_contacts.rb
@@ -0,0 +1,15 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.integer :user_id
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :address_extra
+      t.integer :zipcode, limit: 5
+      t.string :city
+      t.string :state
+      t.string :country
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308184854) do
+ActiveRecord::Schema.define(version: 20140308185915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: true do |t|
+    t.integer "user_id"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.string  "address"
+    t.string  "address_extra"
+    t.integer "zipcode",       limit: 8
+    t.string  "city"
+    t.string  "state"
+    t.string  "country"
+  end
 
   create_table "subscriptions", force: true do |t|
     t.string   "email"
