ad31a83c3607f96c1a0f53ae81a65418e7489664
Devin Brown <devin.g.brown@gmail.com>
2014-03-08 12:29:31 -0700

    Add confirmable + lockable attributes on user model.

--- db/schema.rb
+++ db/schema.rb
@@ -28,12 +28,6 @@ ActiveRecord::Schema.define(version: 20140308185915) do
     t.string  "country"
   end
 
-  create_table "subscriptions", force: true do |t|
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -45,12 +39,21 @@ ActiveRecord::Schema.define(version: 20140308185915) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "type_id"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
