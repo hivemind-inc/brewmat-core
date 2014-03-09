eaa2890de2036d1ad4d33fa6f8ed5e85b521bf8e
Devin Brown <devin.g.brown@gmail.com>
2014-03-09 14:39:24 -0600

    Add price to transaction table (total)

--- db/migrate/20140308194939_create_transactions.rb
+++ db/migrate/20140308194939_create_transactions.rb
@@ -3,6 +3,7 @@ class CreateTransactions < ActiveRecord::Migration
     create_table :transactions do |t|
       t.belongs_to :user
 
+      t.decimal :price, :precision => 8, :scale => 2
       t.integer :state_id, null: false
     end
   end
