387062566464127e12ca3bb5a0aed66721e01370
Devin Brown <devin.g.brown@gmail.com>
2014-03-09 14:45:03 -0600

    null: false for user_id in transaction table

--- db/migrate/20140308194939_create_transactions.rb
+++ db/migrate/20140308194939_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
-      t.belongs_to :user
+      t.belongs_to :user, null: false
 
       t.decimal :price, :precision => 8, :scale => 2
       t.integer :state_id, null: false
