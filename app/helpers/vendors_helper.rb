9f6d0d2639d15c294043cc349b002d1e436a4174
Devin Brown <devin.g.brown@gmail.com>
2014-03-22 08:14:41 -0600

    Load vendor hero + logo via the DB.

--- app/helpers/vendors_helper.rb
+++ app/helpers/vendors_helper.rb
@@ -1,2 +1,9 @@
 module VendorsHelper
+  def vendor_hero_image(v)
+    v.images.where(image_type: "hero").first.url
+  end
+
+  def vendor_logo_image(v)
+    v.images.where(image_type: "logo").first.url
+  end
 end
