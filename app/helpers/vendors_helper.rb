6652dc11335c51c6632e49963258087f1caf5edb
Devin Brown <devin.g.brown@gmail.com>
2014-03-23 12:11:32 -0600

    Don't attempt to load vendor image if they don't exist.

--- app/helpers/vendors_helper.rb
+++ app/helpers/vendors_helper.rb
@@ -1,9 +1,9 @@
 module VendorsHelper
   def vendor_hero_image(v)
-    v.images.where(image_type: "hero").first.url
+    v.images.where(image_type: "hero").first.url unless v.images.empty?
   end
 
   def vendor_logo_image(v)
-    v.images.where(image_type: "logo").first.url
+    v.images.where(image_type: "logo").first.url unless v.images.empty?
   end
 end
