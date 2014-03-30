e5383894b1d1cbc4ccac69ffa8834052d3072a0c
Devin Brown <devin.g.brown@gmail.com>
2014-03-30 17:42:12 -0600

    Tests for splash page; #68507704

---
+++ spec/views/application/splash_spec.html.slim_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'application/splash.html.slim' do
+  before { render }
+
+  subject { rendered }
+
+  it { rendered.should have_css "a[href='https://www.facebook.com/grndz']" }
+  it { rendered.should have_css "a[href='https://twitter.com/grndzbrew']" }
+  it { rendered.should have_content "grndz" }
+  it { rendered.should have_content "brew with confidence." }
+  it { rendered.should render_template('shared/_email_subscribe') }
+end
