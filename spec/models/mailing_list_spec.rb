62192ea35789038795581c79a8770ba5cf22d251
Devin Brown <devin.g.brown@gmail.com>
2014-03-30 17:36:39 -0600

    Add tests for model; #68507704

--- spec/models/mailing_list_spec.rb
+++ spec/models/mailing_list_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe 'mailing_list' do
+describe MailingList do
+  context 'valid email address' do
+    it { MailingList.new(email: "bananas@food.com").should be_valid }
+  end
 
+  context 'invalid email address' do
+    it { MailingList.new().should_not be_valid }
+    it { MailingList.new(email: "bananas").should_not be_valid }
+    it { MailingList.new(email: "(&A*(^A!&@").should_not be_valid }
+  end
 end
