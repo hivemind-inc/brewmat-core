c4d506cee17dcb37b360f3cc8dde3666d56d9570
Devin Brown <devin.g.brown@gmail.com>
2014-03-30 20:54:57 -0600

    Code cleanup.

--- app/helpers/vendors_helper.rb
+++
@@ -1,9 +0,0 @@
-module VendorsHelper
-  def vendor_hero_image(v)
-    v.images.where(image_type: "hero").first.url unless v.images.empty?
-  end
-
-  def vendor_logo_image(v)
-    v.images.where(image_type: "logo").first.url unless v.images.empty?
-  end
-end
