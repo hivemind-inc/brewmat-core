d13a8e27b76a60e5912d7b996ba8ab9d2800404e
Devin Brown <devin.g.brown@gmail.com>
2014-08-10 16:19:23 -0600

    Moved something to somewhere... goodnight...

--- spec/views/application/home.html.haml_spec.rb
+++ spec/views/application/home.html.haml_spec.rb
@@ -1,7 +1,10 @@
 require "spec_helper"
 
 describe "application/home.html.haml" do
-  before { render }
+  before  { render }
+  subject { rendered }
 
-  it { expect(rendered).to have_css "h1", text: "Welcome Home. Buy some coffee."}
+  it { should have_css "h1", text: "Welcome Home. Buy some coffee."}
+  it { should have_css "a[href='#{users_path}']", text: "Users"}
+  it { should have_css "a[href='#{products_path}']", text: "Products"}
 end
--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -3,7 +3,4 @@ require "spec_helper"
 describe "layouts/application.html.haml" do
   before  { render }
   subject { rendered }
-
-  it { should have_css "a[href='#{users_path}']", text: "Users"}
-  it { should have_css "a[href='#{products_path}']", text: "Products"}
 end
