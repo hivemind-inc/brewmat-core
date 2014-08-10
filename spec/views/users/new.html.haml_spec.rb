c5e5b8be3502fc920a9b26d4913f99e072968db5
Devin Brown <devin.g.brown@gmail.com>
2014-08-10 08:41:00 -0600

    user scaffold

---
+++ spec/views/users/new.html.haml_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "users/new" do
+  before(:each) do
+    assign(:user, stub_model(User,
+      :first_name => "MyString",
+      :last_name => "MyString"
+    ).as_new_record)
+  end
+
+  it "renders new user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", users_path, "post" do
+      assert_select "input#user_first_name[name=?]", "user[first_name]"
+      assert_select "input#user_last_name[name=?]", "user[last_name]"
+    end
+  end
+end
